904353cda6658b11c01cdba25f91af10208e9ce6
chinhnd7 <ndchinh110797@gmail.com>
2023-03-02 19:16:07 +0700

    phimmoi.net: tailwind basic

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+
+const Index = () => {
+    return (
+        <h1 className='text-5xl font-bold mt-6 text-cyan-900'>#1 Free Online Movie Streaming Site in Vietnam</h1>
+    )
+}
+
+export default Index
